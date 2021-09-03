5e823a713651ce71757673c47d33c1fefdf4532e
Krushil Naik <krushilnaik96@gmail.com>
2021-09-03 12:22:44 -0400

    Add quiz logic, sample quiz

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,23 +1,53 @@
-const data = {
-	title: 'This is a question',
-	choiceA: 'Choice A',
-	choiceB: 'Choice B',
-	choiceC: 'Choice C',
-	choiceD: 'Choice D',
-	correct: 2
-};
+/**
+ * @type {HTMLDivElement}
+ */
+let root = document.querySelector('.wrapper');
 
 /**
- * @param {string} title - the actual question
- * @param {string} choiceA - choice index 0
- * @param {string} choiceB - choice index 1
- * @param {string} choiceC - choice index 2
- * @param {string} choiceD - choice index 3
- * @param {number} correct - index of the correct choice
+ * @type {HTMLSpanElement}
  */
-function Question(title, choiceA, choiceB, choiceC, choiceD, correct) {
+let progressBar = document.querySelector('.progress-bar span');
+
+/**
+ * @type {HTMLDivElement}
+ */
+let progressMarkers = document.querySelector('.markers');
+
+let answered = 0;
+
+// TODO: this is pretty useless... get rid of it
+let answeredCorrectly = [];
+
+/**
+ * @typedef QuestionShape
+ * @property {string} title - the actual question
+ * @property {string} choiceA - choice with index 0
+ * @property {string} choiceB - choice with index 1
+ * @property {string} choiceC - choice with index 2
+ * @property {string} choiceD - choice with index 3
+ * @property {number} correct - the correct choice's index
+ */
+
+/**
+ * @param {QuestionShape} question
+ * @param {number} currentIndex
+ * @param {number} numQuestions
+ */
+function Question(
+	{ title, choiceA, choiceB, choiceC, choiceD, correct },
+	currentIndex,
+	numQuestions
+) {
 	let question = document.createElement('div');
 	question.className = 'question';
+
+	const offset = (numQuestions - currentIndex - 1) * 10;
+
+	const margin = `-${offset}px`;
+
+	question.style.marginTop = margin;
+	question.style.marginLeft = margin;
+	question.style.zIndex = currentIndex.toString();
 
 	question.innerHTML = `<h2>${title}</h2>`;
 
@@ -25,11 +55,49 @@ function Question(title, choiceA, choiceB, choiceC, choiceD, correct) {
 		let choice = document.createElement('button');
 		choice.className = 'choice';
 		choice.innerHTML = /*html*/ `
-			<div><span>${text}</span></div>
+			<div>
+				<span>${text}</span>
+			</div>
 		`;
 
 		choice.addEventListener('click', () => {
-			console.log(i === correct ? 'Correct!' : 'Nah');
+			const wasCorrect = i === correct;
+
+			answeredCorrectly[numQuestions - currentIndex] = wasCorrect ? 1 : -1;
+
+			progressMarkers.children[answered].classList.add(wasCorrect ? 'right' : 'wrong');
+
+			question.style.backgroundColor = wasCorrect ? 'mediumseagreen' : 'salmon';
+
+			answered++;
+
+			progressBar.style.width = `${(answered / numQuestions) * 100}%`;
+
+			question.classList.add('answered');
+
+			question.innerHTML += /*html*/ `
+				<div class="blocker">
+					<span>${wasCorrect ? '✓' : 'X'}</span>
+				</div>
+			`;
+
+			setTimeout(() => {
+				question.style.marginTop = `500px`;
+				question.style.opacity = '0';
+			}, 200);
+
+			setTimeout(() => {
+				root.style.top = `${answered * 10}px`;
+				root.style.left = `${answered * 10}px`;
+
+				setTimeout(() => {
+					question.remove();
+				}, 250);
+			}, 300);
+
+			if (answered === numQuestions) {
+				alert(`You finished the quiz with a score of ${answeredCorrectly}`);
+			}
 		});
 
 		question.appendChild(choice);
@@ -38,8 +106,21 @@ function Question(title, choiceA, choiceB, choiceC, choiceD, correct) {
 	return question;
 }
 
-const { title, choiceA, choiceB, choiceC, choiceD, correct } = data;
+fetch('../../sample/sampleQuiz.json')
+	.then((response) => response.json())
+	.then((sampleQuiz) => {
+		const numQuestions = sampleQuiz.length;
 
-document
-	.querySelector('main')
-	.appendChild(Question(title, choiceA, choiceB, choiceC, choiceD, correct));
+		progressMarkers.style.gridTemplateColumns = `repeat(${numQuestions}, 1fr)`;
+
+		answeredCorrectly = Array(numQuestions).fill(0);
+		answeredCorrectly.forEach(() => {
+			progressMarkers.innerHTML += /*html*/ `
+				<span class='tag'></span>
+			`;
+		});
+
+		sampleQuiz.reverse().forEach((question, i) => {
+			root.appendChild(Question(question, i, numQuestions));
+		});
+	});
