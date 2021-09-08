9defeb203ba497b0e755a91e87ed2443939aedb5
Krushil Naik <krushilnaik96@gmail.com>
2021-09-08 12:08:30 -0400

    Add more styling to the leaderboard

--- assets/js/leaderboard.js
+++ assets/js/leaderboard.js
@@ -1,7 +1,29 @@
 let scoresElement = document.getElementById('scores');
 
 function showScores() {
-	scoresElement.innerHTML = localStorage.getItem('quizvar-scores');
+	const scores = JSON.parse(localStorage.getItem('quizvar-scores'));
+
+	const gradients = [
+		'linear-gradient(-45deg, gold, goldenrod)',
+		'linear-gradient(-45deg, aliceblue, gainsboro)',
+		'linear-gradient(-45deg, chocolate, lightsalmon)'
+	];
+
+	scores
+		.sort((a, b) => (a.score > b.score ? -1 : 1))
+		.forEach((score, i) => {
+			scoresElement.innerHTML += /*html*/ `
+				<div class="card">
+					<div class="badge" style="background: ${
+						i < gradients.length ? gradients[i] : 'transparent'
+					}">
+						<img src="assets/images/star.png" alt="star">
+					</div>
+					<span class="player-initials">${score.initials}</span>
+					<span class="number">${score.score}</span>
+				</div>
+			`;
+		});
 }
 
 function clearScores() {
